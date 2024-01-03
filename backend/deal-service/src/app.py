107c9ebea6e2b20931ef3c2f5a2de6e957938a4c
bokuan <bokuan.tang@attsystemsgroup.com>
2024-01-03 23:34:06 +0800

    Add nginx proxy

--- backend/deal-service/src/app.py
+++ backend/deal-service/src/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify
 from web_scraping.scrape import get_webscrape_data
 
-CACHED_DATA = get_webscrape_data()
 app = Flask(__name__)
+
+CACHED_DATA = get_webscrape_data()
 
 @app.route('/get', methods = ['GET'])
 def get_articles():
@@ -12,6 +13,9 @@ def get_articles():
 def say_hello():
     return jsonify({"msg": "Hello from Flask"})
 
+@app.route('/flask-health-check', methods=["GET"])
+def flask_health_check():
+	return "success", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
