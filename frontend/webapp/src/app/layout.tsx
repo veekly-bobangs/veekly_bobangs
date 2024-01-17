8ad9a74f537a05baeb424fcfadfb974135b213e6
bokuan <bokuan.tang@attsystemsgroup.com>
2024-01-17 09:57:44 +0800

    Init nextjs webapp

---
+++ frontend/webapp/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import '@mantine/core/styles.css';
+import React from 'react';
+import { MantineProvider, ColorSchemeScript } from '@mantine/core';
+
+export const metadata = {
+  title: 'Veekly Bobangs',
+  description: 'I am using Mantine with Next.js!',
+};
+
+export default function RootLayout({ children }: { children: any }) {
+  return (
+    <html lang="en">
+      <head>
+        <ColorSchemeScript />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+      </head>
+      <body>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
+    </html>
+  );
+}
