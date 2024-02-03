6f9760eae0ba58587da41659c69b7f7adcc4863d
Tang Bo Kuan <e0701815@u.nus.edu>
2024-02-03 23:10:57 +0800

    Add celery beat and decouple flask from celery

--- backend/deal-service/src/app.py
+++ backend/deal-service/src/app.py
@@ -1,49 +1,29 @@
+import json
+import os
 from flask import Flask, jsonify
-import celery.states as states
 from celeryUtils.makeCelery import celery
 from redis.lock import Lock
-from redisConfig import redisClient, SCRAPE_LOCK, SCRAPE_TASK_ID, SCRAPE_TASK
-import os
+from redisConfig import redisClient, SCRAPE_RES, SCRAPE_RES_TIME
 
 app = Flask(__name__)
-
-def send_scrape_task(retry=False):
-    lock = Lock(redisClient, SCRAPE_LOCK, timeout=30)
-    if lock.acquire(blocking=False):
-        try:
-            if retry:
-                redisClient.delete(SCRAPE_TASK_ID)
-            task_id = redisClient.get(SCRAPE_TASK_ID)
-            if task_id is None:
-                scraping_task = celery.send_task(SCRAPE_TASK)
-                redisClient.set(SCRAPE_TASK_ID, scraping_task.id)
-        except Exception as e:
-            print("Error ending scrape task: ", e)
-        finally:
-            lock.release()
-
-
-# Call send_scrape_task when app starts. The locking mechanism ensures
-# that only one task is sent to celery in production with multiple gunicorn workers
-send_scrape_task()
+# For dockerfiles to access celery
+celery
 
 @app.route('/get', methods = ['GET'])
 def get_articles():
-    scraping_task_id = redisClient.get(SCRAPE_TASK_ID)
-    if scraping_task_id is None:
-        return jsonify({"status": "no scraping task found"})
-    
-    scraping_task_res = celery.AsyncResult(scraping_task_id)
-    if scraping_task_res.state == states.PENDING:
+    scrape_res = redisClient.get(SCRAPE_RES)
+    if scrape_res is None:
         return jsonify({"status": "pending"})
-    elif scraping_task_res.state == states.FAILURE:
-        # retry the task
-        send_scrape_task(retry=True)
-        return jsonify({"status": "failed, retrying"})
-    elif scraping_task_res.state == states.SUCCESS:
-        return jsonify(scraping_task_res.get())
-    else:
-        return jsonify({"status": "unknown- " + scraping_task_res.state})
+    
+    return jsonify(json.loads(scrape_res))
+    
+@app.route('/get-time', methods = ['GET'])
+def get_time():
+    scrape_res_time = redisClient.get(SCRAPE_RES_TIME)
+    if scrape_res_time is None:
+        return jsonify({"status": "pending"})
+    
+    return jsonify({"time": scrape_res_time.decode("utf-8")})
 
 @app.route("/hello", methods=["GET"])
 def say_hello():
