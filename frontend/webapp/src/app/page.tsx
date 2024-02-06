be187a63c8f8bce74a0569f4071623a61282f7c8
bokuan <bokuan.tang@attsystemsgroup.com>
2024-02-06 15:22:15 +0800

    Fix issue where nothing is displayed if no response from backend

--- frontend/webapp/src/app/page.tsx
+++ frontend/webapp/src/app/page.tsx
@@ -1,5 +1,6 @@
 import {
   Stack,
+  Text,
 } from '@mantine/core';
 import { AllDeals, NearbyDeals } from '@/components/homePage'
 import { fetchDealsStatic } from '@/utils/staticDealFetch';
@@ -9,7 +10,10 @@ export default async function HomePage() {
 
   return (
     <Stack gap="lg">
-      {response?.status ? <p>Something is wrong with our server, try again later. status: {response.status}</p> : null}
+      {response?.status ?
+        <Text>Something is wrong with our server, try again later. status: {response.status}</Text>
+        : <Text>Servers are down, please try again later</Text>
+      }
       {response?.deals ? <AllDeals deals={response.deals} /> : null}
       {response?.deals ? <NearbyDeals deals={response.deals} /> : null}
     </Stack>
