940bda8c26ce4114322b34f81a5c2aa08fd6759f
bokuan <bokuan.tang@attsystemsgroup.com>
2024-02-07 17:15:41 +0800

    Update scraping

--- backend/deal-service/src/celeryUtils/makeCelery.py
+++ backend/deal-service/src/celeryUtils/makeCelery.py
@@ -13,9 +13,9 @@ CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localho
 celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
 
 celery.conf.beat_schedule = {
-    'webscrape-every-10-mins': {
+    'webscrape-every-25-mins': {
         'task': SCRAPE_TASK,
-        'schedule': crontab(minute='*/10')
+        'schedule': crontab(minute='*/25')
     }
 }
 celery.conf.timezone = 'UTC'
