21442e45f1e1e11b7a40b5cc8948cc01fdbaa5c2
Tang Bo Kuan <e0701815@u.nus.edu>
2024-09-06 14:41:01 +0800

    Update terraform infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,21 +10,27 @@ terraform {
 }
 
 provider "aws" {
-  region  = "ap-southeast-1"
+  region = "ap-southeast-1"
 }
 
 resource "aws_instance" "veekly_fe_server" {
-  ami           = "ami-0a0e5d9c7acc336f1"
+  ami           = "ami-0497a974f8d5dcef8"
   instance_type = "t2.micro"
 
-  key_name = "aws_kp"
+  key_name = "peerprep"
+
+  security_groups = [aws_security_group.veekly_allow_ssh_http_https.name]
 
   user_data = <<EOF
 #!/bin/bash
+sudo apt-get update
+
 # Install docker
 sudo apt-get install docker.io -y
 
 sudo systemctl start docker
+
+sudo systemctl enable docker
 
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
@@ -35,15 +41,16 @@ sudo usermod -aG docker ubuntu
 sudo systemctl restart docker
 EOF
 
+  
   tags = {
     Name = "Veekly FE VM"
   }
 }
 
 resource "aws_ebs_volume" "veekly_fe_ebs" {
-  availability_zone = aws_instance.app_server.availability_zone
+  availability_zone = aws_instance.veekly_fe_server.availability_zone
   size              = 10
-  type = "gp2"
+  type              = "gp2"
 
   tags = {
     Name = "Veekly FE EBS"
@@ -52,6 +59,47 @@ resource "aws_ebs_volume" "veekly_fe_ebs" {
 
 resource "aws_volume_attachment" "attach_ebs" {
   device_name = "/dev/sdh"
-  volume_id   = aws_ebs_volume.myebs.id
-  instance_id = aws_instance.app_server.id
+  volume_id   = aws_ebs_volume.veekly_fe_ebs.id
+  instance_id = aws_instance.veekly_fe_server.id
+}
+
+resource "aws_security_group" "veekly_allow_ssh_http_https" {
+  name        = "veekly allow_ssh_http_https"
+  description = "Allow SSH, HTTP, and HTTPS inbound traffic"
+
+  # Allow SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Open to all IPs, you can restrict it for security
+  }
+
+  # Allow HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Open to all IPs
+  }
+
+  # Allow HTTPS
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Open to all IPs
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Allow SSH, HTTP, HTTPS"
+  }
 }
